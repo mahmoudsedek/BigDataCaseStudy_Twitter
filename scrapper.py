12163be4fcc0b160b6c5e22a8c53358d4ed118ca
Mahmoud Sedek <75182825+mahmoudsedek@users.noreply.github.com>
2022-04-14 12:45:21 +0200

    Update scrapper.py

--- scrapper.py
+++ scrapper.py
@@ -73,11 +73,10 @@ while True:
             # sending tweets as it's in records dictionary [or a json object]
             finalRecord = jsonpickle.encode(records)
             producer.send(topic_name, value=finalRecord)
-            sleep(2)  # sending each tweet to kafka-topic after 5 seconds
+            sleep(2)  # sending each tweet to kafka-topic after 2 seconds
 
         else:
             print("No more tweets of topic: " + hashtag_demo.split('-')[0] + ", Keep listening...")
-            sleep(
-                60)  # if there's no more tweets about this #HASHTAG make the application wats for 1 minute before go and search for new data
+            sleep(60)  # if there's no more tweets about this #HASHTAG make the application wats for 1 minute before go and search for new data
         # consider it a pretty way to keep you script and twitter-api credentials safe from not being suspended :'D 
 
